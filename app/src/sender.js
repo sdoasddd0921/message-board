a374d23cae6d056d11ea1d86dda0a7f485ecdb8e
Onion <850307035@qq.com>
2018-03-29 23:50:32 +0800

    sender样式

--- app/src/sender.js
+++ app/src/sender.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './css/sender.css'
 
 class Sender extends React.Component {
+  submit(e) {
+    e.preventDefault();
+    console.log('sub')
+  }
+
   render() {
     return (
-      <div>
-        sender
+      <div id="sender" className="container">
+        <form onSubmit={this.submit}>
+          <textarea name="message" id="message" className="form-control" cols="30" rows="5"></textarea>
+          <p>
+            <label htmlFor="email">邮箱：</label>
+            <input type="email" id="email" className="form-control" placeholder="name@example.com"/>
+            <button type="submit" className="btn btn-primary">留言</button>
+          </p>
+        </form>
       </div>
     );
   }
