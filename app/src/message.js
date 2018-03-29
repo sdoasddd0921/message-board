ece659991a95caf6efa7bf00f889f3399ad599e5
Onion <850307035@qq.com>
2018-03-29 23:55:00 +0800

    开始做messages

--- app/src/message.js
+++ app/src/message.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './css/message.css';
+
+const Test = () => (
+  <div className="box">
+    box
+  </div>
+);
 
 class Messages extends React.Component {
   render() {
     return (
-      <div>
-        message
+      <div id="messages" className="container">
+        <Test />
       </div>
     );
   }
